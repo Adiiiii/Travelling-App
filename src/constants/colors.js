2e96a6845325ea64a1dacf575987bfe5e24185d1
Aditya Saxena <saxenaadi92@gmail.com>
2020-10-04 20:19:52 +0530

    Aditya : Updated color constants

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,3 +2,5 @@ export const PUREWHITE = '#fff';
 export const JUSTBLACK = '#000';
 export const THEMECOLOR = '#f75d38';
 export const BESTGRAY = '#d0cecf';
+export const MEDIUMGRAY = '#7c7c7c';
+export const DULLBLACK = '#545454';
--- src/screens/App.js
+++ src/screens/App.js
@@ -5,6 +5,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import screenNames from '../constants/screenNames';
 import TabNav from '../navigation/discoverTabNavigator';
+import { decode, encode } from 'base-64';
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
 
 const Stack = createStackNavigator();
 const App = () => (
